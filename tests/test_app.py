67084c97189a122c4dec8321a07a33fea525a1c5
m-pasima <theivykierachronicles@gmail.com>
2024-06-18 18:22:32 +0100

    Create test_app.py

---
+++ tests/test_app.py
@@ -0,0 +1,10 @@
+import pytest
+from src.utils import add, subtract
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(-1, 1) == 0
+
+def test_subtract():
+    assert subtract(2, 1) == 1
+    assert subtract(2, 2) == 0
