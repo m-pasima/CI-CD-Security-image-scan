e091ec40251132acd551da0389986bdf4bf5f2ac
m-pasima <theivykierachronicles@gmail.com>
2024-06-18 18:20:29 +0100

    Create app.py

---
+++ src/app.py
@@ -0,0 +1,10 @@
+from utils import add, subtract
+
+def main():
+    a = 10
+    b = 5
+    print(f"Addition of {a} and {b} is {add(a, b)}")
+    print(f"Subtraction of {a} and {b} is {subtract(a, b)}")
+
+if __name__ == "__main__":
+    main()
